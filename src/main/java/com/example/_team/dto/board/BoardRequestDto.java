6f6b780ff88f42ec80214533216fe875a7b89401
정교영 <rydud65@gmail.com>
2024-09-09 09:46:56 +0900

    feat 게시판 crud, 리스트조회#21

--- src/main/java/com/example/_team/dto/board/BoardRequestDto.java
+++ src/main/java/com/example/_team/dto/board/BoardRequestDto.java
@@ -6,7 +6,9 @@ import com.example._team.domain.enums.Category;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @NoArgsConstructor
 public class BoardRequestDto {
