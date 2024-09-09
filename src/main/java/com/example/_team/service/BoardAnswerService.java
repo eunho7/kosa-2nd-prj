c08940100da8c459319083a68a23ac58a9e44a01
Eunho <76768140sa@gmail.com>
2024-09-10 08:50:02 +0900

    feat 게시판 답들 #33

---
+++ src/main/java/com/example/_team/service/BoardAnswerService.java
@@ -0,0 +1,31 @@
+package com.example._team.service;
+
+import com.example._team.domain.Board;
+import com.example._team.domain.Users;
+import com.example._team.dto.board.AnswerRequestDto;
+import com.example._team.dto.board.BoardResponseDto;
+import com.example._team.repository.BoardRepository;
+import com.example._team.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BoardAnswerService {
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+
+    public void saveAnswer(AnswerRequestDto answerRequestDto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        
+        Users user = userRepository.findByEmail(email).get();
+        Board answerBoard = boardRepository.findById(answerRequestDto.getAnswerBoardIdx()).get();
+
+        Board board =  AnswerRequestDto.toSaveAnswerDto(user, answerRequestDto, answerBoard);
+        boardRepository.save(board);
+    }
+}
