e45c669ce95d540dfe2bbdf6744ad92843a31af3
Eunho <76768140sa@gmail.com>
2024-09-12 13:57:52 +0900

    feat 신고 게시판 페이지네이션 #49

--- src/main/java/com/example/_team/repository/ReportsRepository.java
+++ src/main/java/com/example/_team/repository/ReportsRepository.java
@@ -15,6 +15,5 @@ public interface ReportsRepository extends JpaRepository<Reports, Integer> {
     @Query(value = "SELECT * FROM ( SELECT ROWNUM AS RNUM, Z.* FROM (select reports.* from ( reports join board on reports.board_idx = board.board_idx) where board.status = :statusValue order by reports.created_at asc ) Z WHERE  ROWNUM <= :endRow) WHERE  RNUM >= :startRow",
             countQuery = "SELECT COUNT(*) FROM REPORTS",  nativeQuery = true)
     List<Reports> findByStatusAndCreatedAt(@Param("startRow") int startRow, @Param("endRow") int endRow, @Param("statusValue") int statusValue, Pageable pageable);
-
 }
 
