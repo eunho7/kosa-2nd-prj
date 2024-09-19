1177c78532e601591b9d40a7e8e2a0302e8559c6
songzuhee <songjuhee.dev@gmail.com>
2024-09-19 17:13:54 +0900

    fix: 충돌 해결 #66

--- src/main/java/com/example/_team/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/example/_team/config/CustomAuthenticationSuccessHandler.java
@@ -15,7 +15,8 @@ import java.util.Set;
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ROLE_ADMIN")) {
             response.sendRedirect("/admin/reports/list");
@@ -23,8 +24,4 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
             response.sendRedirect("/api/travel/random");
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 54215f4a2f781d4a3132070d7969f01ac67cd5ef
