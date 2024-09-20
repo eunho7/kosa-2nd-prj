5cf6a9152e315499df7d3f13afda96402e0fee2a
zoown12 <zoown12@gmail.com>
2024-09-20 11:50:31 +0900

    fix 파일 충돌 해결

--- PRO/src/main/java/com/example/_team/domain/enums/Category.java
+++
@@ -1,5 +0,0 @@
-package com.example._team.domain.enums;
-
-public enum Category {
-    질문, 정보, 계획, 기타
-}
--- PRO/src/main/java/com/example/_team/repository/BoardFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example._team.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example._team.domain.BoardFiles;
-
-public interface BoardFileRepository extends JpaRepository<BoardFiles, Integer> {
-
-}
